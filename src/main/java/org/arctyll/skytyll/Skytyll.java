0648ff60ceb1445d92cc7472bda22af18bec7d5d
SigmaCat <sigmacat_12@outlook.com>
2025-07-11 10:48:53 -0400

    Update Skytyll.java

--- src/main/java/org/arctyll/skytyll/Skytyll.java
+++ src/main/java/org/arctyll/skytyll/Skytyll.java
@@ -1,1 +1,12 @@
+package org.arctyll.skytyll;
 
+import org.bukkit.plugin.java.JavaPlugin;
+import org.arctyll.skytyll.events.JoinListener;
+
+public class Skytyll extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new JoinListener(), this);
+    }
+}
