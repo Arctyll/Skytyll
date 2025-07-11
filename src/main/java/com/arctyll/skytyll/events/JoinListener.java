af5c88599a7e0d3a1011d0dec18f1a5c44d4035f
SigmaCat <sigmacat_12@outlook.com>
2025-07-11 12:52:49 -0400

    Create JoinListener.java

---
+++ src/main/java/com/arctyll/skytyll/events/JoinListener.java
@@ -0,0 +1,32 @@
+package com.arctyll.skytyll.events;
+
+import com.arctyll.skytyll.Skytyll;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class JoinListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                player.sendMessage("§eWelcome to §aSkyBlock!");
+
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        player.sendMessage("§b20% experience multiplier is currently enabled!");
+                        player.sendMessage("§d+10 Magic Find Bonus!");
+                    }
+                }.runTaskLater(Skytyll.getInstance(), 20L);
+            }
+        }.runTaskLater(Skytyll.getInstance(), 40L);
+    }
+}
