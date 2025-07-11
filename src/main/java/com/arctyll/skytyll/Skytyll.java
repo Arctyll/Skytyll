3b6a0981976fa232889c63d2581449c968c38d17
SigmaCat <sigmacat_12@outlook.com>
2025-07-11 13:08:35 -0400

    Update Skytyll.java

--- src/main/java/com/arctyll/skytyll/Skytyll.java
+++ src/main/java/com/arctyll/skytyll/Skytyll.java
@@ -1,6 +1,7 @@
 package com.arctyll.skytyll;
 
 import com.arctyll.skytyll.events.JoinListener;
+import com.arctyll.skytyll.events.PlayerHungerListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Skytyll extends JavaPlugin {
@@ -12,6 +13,7 @@ public class Skytyll extends JavaPlugin {
         instance = this;
 
         getServer().getPluginManager().registerEvents(new JoinListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerHungerListener(), this);
     }
 
     public static Skytyll getInstance() {
