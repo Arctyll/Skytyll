1ac21a6d2dcb7f7cd212b7a67bc9a4a8e7547b68
ClassyCoder <classycoder1@gmail.com>
2025-07-11 20:45:29 +0530

    Initial Project Setup

---
+++ src/main/java/com/arctyll/skytyll/Skytyll.java
@@ -0,0 +1,18 @@
+package com.arctyll.skytyll;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Skytyll extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// TODO: Implement this method
+		super.onEnable();
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO: Implement this method
+		super.onDisable();
+	}
+}
