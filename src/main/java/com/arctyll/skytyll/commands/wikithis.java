c4754d21324fd075142e1cd548533d9c3e97d092
SigmaCat <sigmacat_12@outlook.com>
2025-07-12 05:29:45 -0400

    fixed bald cot's code

--- src/main/java/com/arctyll/skytyll/commands/wikithis.java
+++ src/main/java/com/arctyll/skytyll/commands/wikithis.java
@@ -1,11 +1,16 @@
 package com.arctyll.skytyll.commands;
 
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class wikithis implements CommandExecutor {
 
@@ -17,27 +22,25 @@ public class wikithis implements CommandExecutor {
         Player player = (Player) sender;
         ItemStack item = player.getInventory().getItemInMainHand();
 
-        if (item == null) return true;
+        if (item == null || !item.hasItemMeta() || !item.getItemMeta().hasDisplayName()) return true;
 
-        String raw = item.getItemMeta().getDisplayName();
+        ItemMeta meta = item.getItemMeta();
+        String raw = meta.getDisplayName();
         String stripped = ChatColor.stripColor(raw);
         String name = stripped.replace(" ", "_");
 
         TextComponent component = new TextComponent(ChatColor.GRAY + "Click ");
-        TextComponent link = new TextComponent(ChatColor.YELLOW + ChatColor.BOLD + "HERE");
+        TextComponent link = new TextComponent(ChatColor.YELLOW.toString() + ChatColor.BOLD + "HERE");
         link.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://hypixel-skyblock.fandom.com/wiki/" + name));
-        link.setHoverEvent(new HoverEvent(
-                HoverEvent.Action.SHOW_TEXT,
-                new ComponentBuilder(
-                        ChatColor.GRAY + "View " + ChatColor.YELLOW + stripped + ChatColor.GRAY + " on the " + ChatColor.GOLD + "Official SkyBlock Wiki!"
-                ).create()));
+        link.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(ChatColor.GRAY + "View " + ChatColor.YELLOW + stripped + ChatColor.GRAY + " on the " + ChatColor.GOLD + "Official SkyBlock Wiki!").create()));
         component.addExtra(link);
-        component.addExtra(ChatColor.GRAY + " to find it on the" + ChatColor.GOLD + " Official SkyBlock Wiki" ChatColor.GRAY + "!");
-        
+        component.addExtra(new TextComponent(ChatColor.GRAY + " to find it on the" + ChatColor.GOLD + " Official SkyBlock Wiki" + ChatColor.GRAY + "!"));
+
         player.sendMessage("");
         player.sendMessage(ChatColor.GRAY + "Found Item: " + stripped);
         player.spigot().sendMessage(component);
         player.sendMessage("");
+
         return true;
     }
 }
